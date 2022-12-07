e29f7b108c645eb74723f4a893c1db4efe705d90
bange-git <sebastienbange@gmail.com>
2022-12-07 05:55:42 +0100

    added the design for inputing and listing goals

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Button title='Tap Me'  />
+      <View style={styles.goalContainer}>
+        <TextInput style={styles.textInput}>Your Course Goals!</TextInput>
+        <Button title="Add Goals" />
+      </View>
+      <View style={styles.goalList}>
+        <Text>List of Goals ...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 50,
+    paddingHorizontal: 16,
+    flex: 1
   },
+  goalContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingBottom: 20,
+    alignItems: 'center',
+    flex: 1
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalList : {
+    marginTop: 50,
+    flex: 4
+  }
 });
